4c4bd2708eec8ed44a74cd5da6dd01838233cf12
ekaterina <rudkate21@mail.ru>
2023-02-16 13:35:45 +0300

    create task

---
+++ Task/Program.cs
@@ -0,0 +1,41 @@
+﻿// Написать программу, которая из имеющегося массива строк, формирует массив из строк, длина которых меньше или равна 3 символам.
+
+int getUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int userData = int.Parse (Console.ReadLine()!);
+    return userData;
+}
+
+string[] createArray(int lenght)
+{
+    string[] result = new string[lenght];
+    for (int i=0; i< lenght; i++)
+    {
+        result[i]= Console.ReadLine();
+    }
+    return result;
+}
+
+void CountSymbol(int lenght, string[] arr)
+{
+    string[] new_arr = new string[lenght];
+    Console.Write("[");
+    for (int i=0; i< arr.Length; i++)
+    {
+        if (arr[i].Length<=3) Console.Write($"{arr[i]}, ");
+    }
+    Console.Write("]");
+}
+
+int lenght = getUserData("Введите количество строк в массиве");
+string[] array = createArray(lenght);
+
+Console.WriteLine();
+Console.WriteLine("Небольше трех символов:");
+CountSymbol(lenght, array);
+
+
+
